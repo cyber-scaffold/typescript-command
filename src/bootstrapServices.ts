f4a2b7a8a7dbe18905c74e3b9862920c19d84479
frank.leo <1542874601@qq.com>
2024-07-12 10:22:14 +0800

    temp:临时节点标记 星期五 上午 10:22:13 七月 12日 2024 修改了 src/bootstrapServices.ts

--- src/bootstrapServices.ts
+++ src/bootstrapServices.ts
@@ -1,15 +1,14 @@
 import { IOCContainer } from "@/commons/Application/IOCContainer";
 
-import { SessionInfoService } from "@/services/SessionInfoService";
 import { TestService } from "@/services/TestService";
+import { SessionInfoService } from "@/services/SessionInfoService";
 
 import { RequestFactoryService, RequestFactoryServiceFactory } from "@/services/RequestFactoryService";
 import { TransientFactoryService, TransientFactoryServiceFactory } from "@/services/TransientFactoryService";
 
 export async function bootstrapServices() {
-  IOCContainer.bind(SessionInfoService).toSelf().inRequestScope();
-
   IOCContainer.bind(TestService).toSelf().inRequestScope();
+  IOCContainer.bind(SessionInfoService).toSelf().inRequestScope();
 
   IOCContainer.bind(RequestFactoryService).toSelf().inTransientScope();
   IOCContainer.bind(RequestFactoryServiceFactory).toFactory(RequestFactoryServiceFactory);
