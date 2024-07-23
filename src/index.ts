a8c1916ba9d308e135948a9a60d59b2a75ce01e5
frank.leo <1542874601@qq.com>
2024-07-23 10:51:28 +0800

    temp:临时节点标记 星期二 上午 10:51:28 七月 23日 2024 修改了 src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ import { bootstrapServices } from "@/bootstrapServices";
 
 import { definition as definitionTestProjectCommand } from "@/controllers/TestProjectController";
 
-import { name } from "../package.json";
+import { name, version } from "../package.json";
 
 setImmediate(async () => {
   try {
     await bootstrapApplication();
     await bootstrapServices();
-    program.name(green(name)).usage(["", green(`${name} command <argument> [options]`)].join("\n"));
+    program.name(green(name)).usage(["", green(`${name} command <argument> [options]`)].join("\n")).version(version);
     await bootstrapControllers();
 
     const testCommand = program.command("test").description("这是test相关的命令,是根节点");
