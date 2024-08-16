fe07de8d21bab8f0c736b05421a559967b4c9da2
frank.leo <1542874601@qq.com>
2024-08-16 20:15:27 +0800

    temp:临时节点标记 星期五 晚上 8:15:27 八月 16日 2024 删除了 src/bootstrapApplication.ts 删除了 src/bootstrapControllers.ts 删除了 src/bootstrapServices.ts

--- src/bootstrapApplication.ts
+++
@@ -1,9 +0,0 @@
-import { IOCContainer } from "@/commons/Application/IOCContainer";
-
-import { ApplicationConfigManager } from "@/commons/Application/ApplicationConfigManager";
-
-export async function bootstrapApplication() {
-  IOCContainer.bind(ApplicationConfigManager).toSelf().inSingletonScope();
-  await IOCContainer.get(ApplicationConfigManager).initialize();
-
-};
--- src/bootstrapControllers.ts
+++
@@ -1,8 +0,0 @@
-import { IOCContainer } from "@/commons/Application/IOCContainer";
-
-/** 
- * 如果存在全局单例模式或者瞬态模式的Controller的话需要在这里注册 
- * **/
-export async function bootstrapControllers() {
-
-};
