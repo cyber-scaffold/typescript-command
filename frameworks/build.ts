d03df08f40a496a51258d101c6e53ceae51d5ac2
frankleo <1542874601@qq.com>
2026-03-30 04:15:01 +0800

    update framework build script

--- frameworks/build.ts
+++ frameworks/build.ts
@@ -1,8 +1,11 @@
+import fs from "fs";
 import path from "path";
 import esbuild from "esbuild";
+import { promisify } from "util";
 import { nodeExternalsPlugin } from "esbuild-node-externals";
 
 setImmediate(async () => {
+  await promisify(fs.rm)(path.resolve(process.cwd(), "./dist/"), { recursive: true, force: true });
   await esbuild.build({
     entryPoints: [path.resolve(process.cwd(), "./src/index.ts")],
     bundle: true,
